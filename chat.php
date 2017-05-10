878171bc0b6db49f80ff28e5fcffb6c98cb8c029
DESKTOP-M45SVAV\Denny Syaputra <denny.s@mhs.unsyiah.ac.id>
2017-05-10 22:59:39 +0700

    tambah halaman chat

---
+++ chat.php
@@ -0,0 +1,126 @@
+<style media="screen">
+
+  body{
+    background: url("images/chat_background.jpg") no-repeat center center fixed;
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+  }
+
+  .me{
+    background-color:#74a8fc;
+    border: 1px solid;
+    min-height: 6%;
+    margin-right: 0px;
+    font-family: "comic sans ms";
+    border-radius: 8px 0px 8px 8px;
+
+  }
+
+  .friend{
+    background-color:#b1c9ef;
+    border: 1px solid;
+    min-height: 6%;
+    font-family: "comic sans ms";
+    border-radius: 0px 8px 8px 8px;
+
+  }
+
+  @media screen and (max-width: 780px){
+    .me{
+      clear:both;
+      position: relative;
+      right:50px;
+      width:400px;
+      margin:0
+    }
+  }
+
+</style>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Personal Chat</title>
+
+    <!-- Bootstrap -->
+    <link href="bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <br>
+    <div class="row">
+      <div class="col-xs-12 col-sm-12 col-md-12" >
+        <?php
+          require_once "koneksi.php";
+
+          $result=$conn->query("select * from chat where username='".$_COOKIE['user']."' and friend='".$_GET['teman']."' or username='".$_GET['teman']."' and friend='".$_COOKIE['user']."' order by waktu");
+          if($result->num_rows>0){
+            while($pesan=$result->fetch_assoc()){
+              if($pesan["username"]!=$_COOKIE["user"]){
+                echo '
+                    <div class="row" style="width:100%;padding-right:0">
+                      <div class="col-xs-2 col-sm-1 col-md-1" style="padding-right:0;padding-left:25px;width:75px;">
+                        <img src="images/user.png" style="width:45px">
+                      </div>
+                      <div class="col-xs-10 col-sm-11 col-md-11" style="padding-left:0;width:600px;">
+                        <div class="friend">
+                          '.$pesan["pesan"].'
+                        </div>
+                      </div>
+                    </div><br>';
+                  }
+                  else{
+                    echo '
+                    <div class="row" style="width:100%;padding-right:0">
+                      <div class="col-xs-2 col-sm-1 col-md-1 pull-right" style="padding-left:0;padding-right:0;width:50px;">
+                        <img src="images/user.png" style="width:45px;padding-right:0" class="pull-right">
+                      </div>
+                      <div class="col-xs-12 col-sm-11 col-md-11 pull-right" style="width:600px;padding:0">
+                        <div class="me" class="text-align:right;" style="margin-right:0;">
+                          '.$pesan["pesan"].'
+                        </div>
+                      </div>
+                    </div><br>';
+                  }
+                }
+          }
+          else{
+            echo "Tidak Ada Pesan";
+          }
+        ?>
+        <br><br>
+      </div>
+    </div>
+    <div class="row" style="padding:0 20px;">
+      <div class="col-md-12">
+        <form action="send.php" method="post">
+          <div class="form-group">
+            <?php
+              echo'
+                <input type="text" value="'.$_GET["teman"].'" name="teman" hidden="">';
+            ?>
+            <textarea class="form-control" rows="4" placeholder="Pesan Anda" name="pesan" required=""></textarea>
+          </div>
+          <button type="submit" class="btn btn-primary pull-right" name="submit">Kirim</button>
+        </form>
+      </div>
+    </div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="bootstrap.min.js"></script>
+  </body>
+</html>
