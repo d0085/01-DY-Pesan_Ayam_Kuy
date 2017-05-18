09a817e49dc5896ac3663edde15931766b43d6b2
LAPTOP-CSNIP4EB\acer <rahmat.hd88@gmail.com>
2017-05-19 04:01:33 +0700

    hapus beberapa dan edit file cek dan tambah stock penjual

---
+++ prosesReqStock.php
@@ -0,0 +1,23 @@
+<?php
+	session_start();
+	include 'koneksi.php';
+
+	$username=$_SESSION['USERNAME'];
+	$jumlah = $_POST['jumlah'];
+
+	$query="SELECT * FROM akun WHERE peran ='1'";
+	$hasil= mysqli_query($conn, $query);
+	$row=mysqli_fetch_assoc($hasil);
+	$pemasok = $row['username'];
+
+
+	$query2 = "INSERT INTO `transaksi`(`jml_permintaan`, `req_id`, `acc_id`) VALUES ('$jumlah', '$username', '$pemasok')";
+	$hasil2 = mysqli_query($conn, $query2);
+
+	if($hasil){
+		echo '<script>alert("Permintaan stock sudah di proses menuggu tanggapan dari pemasok");location.replace("stockPenjual.php");</script>';		
+	}
+	else {
+		echo '<script>alert("Permintaan stock gagal diproses");location.replace("stockPenjual.php");</script>';
+	}
+?>
