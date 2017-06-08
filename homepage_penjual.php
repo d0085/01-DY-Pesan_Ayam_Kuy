7043b8c0c417ed406e64e9a008ebbfcbaad1fad8
LAPTOP-CSNIP4EB\acer <rahmat.hd88@gmail.com>
2017-06-08 22:48:58 +0700

    edit

--- homepage_penjual.php
+++ homepage_penjual.php
@@ -170,7 +170,7 @@
 				<li><a href="#home">Home</a></li>
 				<li><a href="#about">About</a></li>
 				<li><a href="#contacs">Contacts</a></li>
-				<li><a href="chat_index.php">Chat</a></li>
+				<li><a href="chat_index.php"><span class="icon-large icon-chat"></span> Chat</a></li>
 			</ul>
 			<ul class="nav navbar-nav navbar-right">
 				<li><a href="permintaan_dari_pembeli.php"><span class="glyphicon glyphicon-bell notification-icon"><span id="jumlah">0</span></span></a></li>
